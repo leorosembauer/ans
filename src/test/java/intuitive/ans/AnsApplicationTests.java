795841af652b96327080870494ca1ec8ce72fc55
leorosembauer <leonidasrosembauer@gmail.com>
2025-03-26 15:26:27 -0300

    Primeiro commit

---
+++ src/test/java/intuitive/ans/AnsApplicationTests.java
@@ -0,0 +1,13 @@
+package intuitive.ans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
